ec1764767386fc01141fea846d9bb4bdc66cea43
Hayyi Shinshan Mahesa <hayyi123sm@gmail.com>
2026-04-23 00:10:53 +0800

    second commit in repo asta

--- config/livewire.php
+++ config/livewire.php
@@ -134,9 +134,22 @@ return [
         'directory' => null,                                  // Example: 'tmp'                     | Default: 'livewire-tmp'
         'middleware' => null,                                 // Example: 'throttle:5,1'            | Default: 'throttle:60,1'
         'preview_mimes' => [                                  // Supported file types for temporary pre-signed file URLs...
-            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
-            'mov', 'avi', 'wmv', 'mp3', 'm4a',
-            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
+            'png',
+            'gif',
+            'bmp',
+            'svg',
+            'wav',
+            'mp4',
+            'mov',
+            'avi',
+            'wmv',
+            'mp3',
+            'm4a',
+            'jpg',
+            'jpeg',
+            'mpga',
+            'webp',
+            'wma',
         ],
         'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
         'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
@@ -151,7 +164,7 @@ return [
     | after a redirect has been triggered using something like `redirect(...)`
     | Setting this to true will render the view once more before redirecting
     |
-    */a
+    */
 
     'render_on_redirect' => false,
 
